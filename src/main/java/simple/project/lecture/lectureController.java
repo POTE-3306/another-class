029f5a51a5d50d4724149d42521256f26545085a
rlaalst22 <rlaalst22@gmail.com>
2023-06-17 18:27:29 +0900

    attend 컴포넌트 제거

--- src/main/java/simple/project/lecture/lectureController.java
+++ src/main/java/simple/project/lecture/lectureController.java
@@ -243,31 +243,31 @@ public class lectureController {
         }
         return "class/manage";
     }
-    @RequestMapping("{class_id}/attend")
-    public String managePage(
-            HttpSession session,
-            Model model,
-            @PathVariable("class_id") String classId
-    ){
-        String token = (String) session.getAttribute("token");
-        if (token == null) {
-            return "index";
-        }
-        try {
-            Claims claims = jwToken.getClaims(token);
-            User user = userService.getUserByToken(claims);
-            Course course = courseService.getCourseById(Integer.parseInt(classId));
-
-            if (user == null) {
-                return "index";
-            }
-            model.addAttribute("user", user);
-            model.addAttribute("course", course);
-
-        } catch (Exception e){
-            e.printStackTrace();
-            return "index";
-        }
-        return "class/manage";
-    }
+//    @RequestMapping("{class_id}/attend")
+//    public String managePage(
+//            HttpSession session,
+//            Model model,
+//            @PathVariable("class_id") String classId
+//    ){
+//        String token = (String) session.getAttribute("token");
+//        if (token == null) {
+//            return "index";
+//        }
+//        try {
+//            Claims claims = jwToken.getClaims(token);
+//            User user = userService.getUserByToken(claims);
+//            Course course = courseService.getCourseById(Integer.parseInt(classId));
+//
+//            if (user == null) {
+//                return "index";
+//            }
+//            model.addAttribute("user", user);
+//            model.addAttribute("course", course);
+//
+//        } catch (Exception e){
+//            e.printStackTrace();
+//            return "index";
+//        }
+//        return "class/attend";
+//    }
 }
